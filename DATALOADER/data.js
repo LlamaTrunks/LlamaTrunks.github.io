56aafdcad5dfe8a800c9eab0b4e921dd73b2758f
Mikesael Nieves <97325479+LlamaTrunks@users.noreply.github.com>
2022-02-12 16:39:02 -0500

    Add files via upload

---
+++ DATALOADER/data.js
@@ -0,0 +1,144 @@
+//start button
+function start() {
+    //testing start button functionality
+    console.log("START button pushed!");
+    //let user know that data is being processed
+    document.getElementById("data").rows["seconds"].innerHTML = "Reading Data...";
+    //set index start
+    index = 0;
+    //set interval of timer to update the display
+    timer = setInterval(updateDisplay, time_interval);
+    //disables start button on click event and enables stop
+    document.getElementById("startButton").disabled = true;
+    //ensures stop is enabled on click event
+    document.getElementById("stopButton").disabled = false;
+}
+
+function stop() {
+    clearInterval(timer);
+    //testing stop button functionality
+    console.log("STOP button pushed!");
+    //disables stop button on click event
+    document.getElementById("stopButton").disabled = true;
+    //ensures start button is enabled on click event
+    document.getElementById("startButton").disabled = false;
+}
+
+function sound(src) {
+    this.sound = document.createElement("audio");
+    this.sound.src = src;
+    this.play = function() {
+        this.sound.play();
+    }
+    // this.stop = function() {
+    //     this.sound.pause();
+    // }
+}
+
+
+//define datarow and construct inputs for each row
+function dataRow(legend, value, units) {
+    msg = "<td>";
+    msg += legend;
+    msg += ": </td><td>";
+    msg += value;
+    msg += " " + units;
+    msg += "</td>";
+    return msg;
+}
+//get data from raw datasource
+function getData() {
+    var loadedData = loadData();
+    return loadedData;
+}
+
+class InputData {
+    //constructor to create keywords to access each datapoint
+    constructor(
+        //seconds elapsed data
+        time_seconds,
+        //latitude data
+        latitude,
+        //longitude data
+        longitude,
+        //altitude data from gps sensor
+        gps_altitude,
+        //altitude data from BMP sensor
+        bmpSensor_altitude,
+        //pressure data from BMP Sensor
+        bmpSensor_pressure,
+        //temperature data from BMP Sensor
+        bmpSensor_temp,
+        //temperature data from digital sensor
+        digiSensor_temp,
+        //temperature data from CSS Sensor
+        cssSensor_temp,
+        //eco2 data from CSS Sensor
+        cssSensor_eco2,
+        //tvoc data from CSS Sensor
+        cssSensor_tvoc,
+        //UV data
+        UV,
+        //X coordinates from Acceleromoter
+        accelX,
+        //Y coordinates from Acceleromoter
+        accelY,
+        //Z coordinates from Acceleromoter
+        accelZ,
+        //X coordinates from magnetometer
+        magneticX,
+        //Y coordinates from magnetometer
+        magneticY,
+        //Z coordinates from magnetometer
+        magneticZ,
+        //X coordinates from gyroscope
+        gyroX,
+        //Y coordinates from gyroscope
+        gyroY,
+        //Z coordinates from gyroscope
+        gyroZ
+    ) {
+        //instantiate time_seconds object
+        this.time_seconds = time_seconds;
+         //instantiate latitude object
+        this.latitude = latitude;
+         //instantiate longitude object
+        this.longitude = longitude;
+         //instantiate gps_altitude object
+        this.gps_altitude = gps_altitude;
+         //instantiate bmpSensor_altitude object
+        this.bmpSensor_altitude = bmpSensor_altitude;
+         //instantiate bmpSensor_pressure object
+        this.bmpSensor_pressure = bmpSensor_pressure;
+         //instantiate bmpSensor_temp object
+        this.bmpSensor_temp = bmpSensor_temp;
+         //instantiate digiSensor_temp object
+        this.digiSensor_temp = digiSensor_temp;
+         //instantiate cssSensor_temp object
+        this.cssSensor_temp = cssSensor_temp;
+         //instantiate cssSensor_eco2 object
+        this.cssSensor_eco2 = cssSensor_eco2;
+         //instantiate cssSensor_tvoc object
+        this.cssSensor_tvoc = cssSensor_tvoc;
+         //instantiate UV object
+        this.UV = UV;
+         //instantiate accelX object
+        this.accelX = accelX;
+         //instantiate accelY object
+        this.accelY = accelY;
+         //instantiate accelZ object
+        this.accelZ = accelZ;
+         //instantiate magneticX object
+        this.magneticX = magneticX;
+         //instantiate magneticY object
+        this.magneticY = magneticY;
+         //instantiate magneticZ object
+        this.magneticZ = magneticZ;
+         //instantiate gyroX object
+        this.gyroX = gyroX;
+         //instantiate gyroY object
+        this.gyroY = gyroY;
+         //instantiate gyroZ object
+        this.gyroZ = gyroZ;
+    }
+}
